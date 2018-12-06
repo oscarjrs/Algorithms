7fe7651aa963e300832f918bd2bc9f0d0950e515
Oscar Ramos <oscarjrs7@gmail.com>
2018-12-06 14:05:31 -0500

    Uses Vectors instead of arrays

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -1,23 +1,39 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+
+//int * sort(input[]);
 
 int main()
 {
 	const int ARRAY_SIZE = 10;
-	int data[ARRAY_SIZE];
+	//int data[ARRAY_SIZE];
+	//int sorted[ARRAY_SIZE];
+	
+	std::vector<int> data;
 	
 	std::cout << "Sorting Algorithms. Version 0.1" << std::endl;
 	std::cout << "Please enter 10 numbers:" << std::endl;
 	
 	for(int i=0;i<ARRAY_SIZE;i++)
 	{
-		std::cin >> data[i];
+		int numberEntered;
+		std::cin >> numberEntered;
+		data.push_back(numberEntered);
 		std::cout << "Number entered: " + std::to_string(data[i]) << std::endl;
 	}
+	
+	//sorted = sort(data);
+	for(int i=0; i<data.size(); ++i)
+		std::cout << data[i] << ' ';
+	
+	
+	
 	
 	
 	
 	
     return 0;
 }
+
