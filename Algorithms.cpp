d2fe4c6ba80f2a0de4c5604ce90efdbd42cdd8d2
Oscar Ramos <oscarjrs7@gmail.com>
2018-12-06 12:33:11 -0500

    Expands number of inputs

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -4,11 +4,11 @@
 
 int main()
 {
-	const int ARRAY_SIZE = 5;
+	const int ARRAY_SIZE = 10;
 	int data[ARRAY_SIZE];
 	
 	std::cout << "Sorting Algorithms. Version 0.1" << std::endl;
-	std::cout << "Please enter 5 numbers:" << std::endl;
+	std::cout << "Please enter 10 numbers:" << std::endl;
 	
 	for(int i=0;i<ARRAY_SIZE;i++)
 	{
